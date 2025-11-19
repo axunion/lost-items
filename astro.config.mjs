e1768c583d6e4f68a6e8f55bea93ac9cfa71c790
axunion <120321953+axunion@users.noreply.github.com>
2025-11-19 23:53:33 +0900

    refactor: migrate monorepo to single configuration setup

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { fileURLToPath, URL } from "node:url";
+import cloudflare from "@astrojs/cloudflare";
+import solidJs from "@astrojs/solid-js";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+	adapter: cloudflare(),
+	integrations: [solidJs()],
+
+	vite: {
+		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				"@": fileURLToPath(new URL("./src", import.meta.url)),
+			},
+		},
+	},
+});
